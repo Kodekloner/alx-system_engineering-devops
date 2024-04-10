ba26c8cb529bcfa027f053c8750e1d8a018c8b5f
Kodekloner <billgateokoye@gmail.com>
2024-04-10 02:52:42 +0000

    Updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,30 +1,19 @@
 #!/usr/bin/python3
-'''
-    this module contains the function number_of_subscribers
-'''
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
+
 import requests
-from sys import argv
 
 
 def number_of_subscribers(subreddit):
-    '''
-        returns the number of subscribers for a given subreddit
-    '''
-    headers = {'User-Agent': 'web application:intranet.alxswe.com:v1.0 (by /u/Bale237)'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json
-            return data['data']['subscribers']
-        else:
-            return 0
-    except requests.exceptions.HTTPError as http_err:
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
-    except requests.exceptions.RequestException as e:
-        return 0
-    except ValueError as json_err:
-        return 0
-
-if __name__ == "__main__":
-    number_of_subscribers(argv[1])
